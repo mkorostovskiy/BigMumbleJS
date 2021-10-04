b8f0361a260390a2d5d0e5d0ca885e21dc85f138
Mark Korostovskiy <MKorostovskiy@outlook.com>
2021-10-04 22:07:22 +0200

    Update run.sh
    Corrected a typo

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author : Mark Korostovskiy
 # Email : mark.korostovskiy@stud.hs-bochum.de
-# Content : This script will start all the necessarry files to get the BigMumbleJS-Bot running
+# Content : This script will start all the necessary files to get the BigMumbleJS-Bot running
 # Version : 1.0
 
 node ./src/mumble.js & node ./src/scraper.js
